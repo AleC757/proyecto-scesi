a615b5dce799e62bed2b71a384185254377f2a20
Alejandro Caceres <ucaceresalejandro757@gmail.com>
2026-06-25 17:35:14 -0400

    feat: search drinks for their ids

--- server.js
+++ server.js
@@ -1,24 +1,47 @@
 const express = require('express');
 const app = express();
-app.get('/',(req, res) =>{
-    res.send('Hola mundo');
-});
-app.get('/bebidas',(req, res) =>
-{
-    res.json(
-        [
-            {
-                nombre: "Paceña",
-                grado_alcoholico: 4.8
-            },
-            {
-                nombre: "Singani",
-                grado_alcoholico: 40
-            }
-        ]
-    );
-}
-);
+
+
+const bebidas = [
+    {
+        id: 1,
+        nombre: "Paceña",
+        graduacion: 4.8
+    },
+    {
+        id: 2,
+        nombre: "Singani",
+        graduacion: 40
+    },
+    {
+        id: 3,
+        nombre: "Fernet",
+        graduacion: 39
+    }
+];
 app.listen(3000,()=> {
     console.log('Servidor ejecuntadose en puerto 3000');
 });
+
+app.get('/',(req, res) =>{
+    res.send('Hola mundo');
+});
+
+app.get('/bebidas',(req,res)=>
+{
+    res.json(bebidas);
+})
+app.get('/bebidas/:id',(req, res) =>
+{
+    const id = parseInt(req.params.id);
+    const bebida = bebidas.find(b=>b.id === id);
+    if(!bebida)
+    {
+        return res.status(404).json({
+            error:" bebida no encontrada"
+        }
+        )
+    }
+    res.json(bebida);
+}
+);
